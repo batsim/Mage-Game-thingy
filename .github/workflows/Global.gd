687a5897e3382e32823abaa6fadc7b7c1c9c3b89
Bat_Sime <41552279+batsim@users.noreply.github.com>
2021-04-05 14:08:51 +0200

    Create Global.gd

---
+++ .github/workflows/Global.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var playerNumber = 0
+
+# this one's simple, it's just here to limit the number of players (I could probably put it in the level script but I'm too lazy to change it)
