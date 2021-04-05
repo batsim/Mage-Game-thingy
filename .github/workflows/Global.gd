c65304949e316c09f3f368e2e9a603f3091dfd8a
Bat_Sime <41552279+batsim@users.noreply.github.com>
2021-04-05 18:04:09 +0200

    Update Global.gd

--- .github/workflows/Global.gd
+++ .github/workflows/Global.gd
@@ -2,4 +2,5 @@ extends Node
 
 var playerNumber = 0
 
-# this one's simple, it's just here to limit the number of players (I could probably put it in the level script but I'm too lazy to change it)
+# isbootinup is useful so that it doesn't play the bootup screen when changing scenes 
+var isbootinup = true
